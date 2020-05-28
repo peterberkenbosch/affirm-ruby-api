be1b4e8fb6a5391066b9f510d05c2b255dc0092c
Peter Berkenbosch <info@peterberkenbosch.nl>
2020-05-28 12:14:57 +0200

    Add Affirm::Struct::Transaction#provider (#10)
    Affirm returns a magic number for the provider used on the platform.
    
    This commit adds a provider method on the transaction to return the proper
    name for the provider.

--- lib/affirm/structs/transaction.rb
+++ lib/affirm/structs/transaction.rb
@@ -12,6 +12,15 @@ module Affirm
       attr_accessor :order_id
       attr_accessor :provider_id
       attr_accessor :status
+
+      def provider
+        case provider_id
+        when 1
+          :affirm
+        when 2
+          :katapult
+        end
+      end
     end
   end
 end
